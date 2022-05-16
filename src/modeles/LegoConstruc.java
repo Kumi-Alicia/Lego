612138e9bf773dc1f18aac183fb04174bb51d198
VoltaJiJi <victor.lagneaux2@gmail.com>
2022-05-16 17:24:48 +0200

    Avancee Serialisation par rapport aux constructions de legos

--- src/modeles/LegoConstruc.java
+++ src/modeles/LegoConstruc.java
@@ -6,19 +6,22 @@ public class LegoConstruc implements Serializable {
 	private static final long serialVersionUID = -3832993424892092913L;
 	public String type;
 	public int taille;
-	public int x,y,z;
+	public double x,y,z;
 	
 	public LegoConstruc() {
 		
 	}
-	public LegoConstruc(String type, int taille, int x, int y, int z) {
+	public LegoConstruc(String type, int taille, double x, double y, double z) {
 		this.type=type;
 		this.taille=taille;
 		this.x=x;
 		this.y=y;
 		this.z=z;
 	}
-
+	public String toString() {
+		return this.type +" "+ this.taille + " " + this.x + " " + this.y +" " + this.z;
+	}
+	
 	public String getType() {
 		return type;
 	}
@@ -35,27 +38,27 @@ public class LegoConstruc implements Serializable {
 		this.taille = taille;
 	}
 
-	public int getX() {
+	public double getX() {
 		return x;
 	}
 
-	public void setX(int x) {
+	public void setX(double x) {
 		this.x = x;
 	}
 
-	public int getY() {
+	public double getY() {
 		return y;
 	}
 
-	public void setY(int y) {
+	public void setY(double y) {
 		this.y = y;
 	}
 
-	public int getZ() {
+	public double getZ() {
 		return z;
 	}
 
-	public void setZ(int z) {
+	public void setZ(double z) {
 		this.z = z;
 	}
 }
