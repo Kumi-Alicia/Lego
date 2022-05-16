43b2f22dcaefd0b169945bad337d804ff01e0240
Kumi-Alicia <alicia.ghanem@gmail.com>
2022-05-16 18:37:02 +0200

    correction page tuto

--- src/controleurs/ControleurQ2.java
+++ src/controleurs/ControleurQ2.java
@@ -18,10 +18,7 @@ public class ControleurQ2 {
 
     @FXML
     void quitter(ActionEvent event) throws IOException {
-    	Parent root = FXMLLoader.load(getClass().getResource("../vues/VuePageTuto.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
+    	this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.stage.close();
     }
 }
