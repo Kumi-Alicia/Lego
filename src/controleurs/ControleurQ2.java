58ebf53d7e8cf610f983e75762e14ee6a72849e4
trebeyro <tristan.rebeyrol@gmail.com>
2022-05-16 18:23:38 +0200

    Créations de toutes les pages tutos

---
+++ src/controleurs/ControleurQ2.java
@@ -0,0 +1,27 @@
+package controleurs;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ControleurQ2 {
+	public Stage stage;
+	public Scene scene;
+	public Parent root;
+
+    @FXML
+    void quitter(ActionEvent event) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("../vues/VuePageTuto.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+    }
+}
--- src/controleurs/ControleurTuto.java
+++ src/controleurs/ControleurTuto.java
@@ -17,36 +17,52 @@ public class ControleurTuto {
 
     @FXML
     void Q1(ActionEvent event) throws IOException {
-    	Parent root = FXMLLoader.load(getClass().getResource("../vues/VueQ1.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../vues/VueQ1.fxml"));
+    	Parent root1 = (Parent) fxmlLoader.load();
+    	Stage stage = new Stage();
+    	stage.setScene(new Scene(root1));
+    	stage.showAndWait();
     }
 
     @FXML
-    void Q2(ActionEvent event) {
-    	
+    void Q2(ActionEvent event) throws IOException {
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../vues/VueQ2.fxml"));
+    	Parent root1 = (Parent) fxmlLoader.load();
+    	Stage stage = new Stage();
+    	stage.setScene(new Scene(root1));
+    	stage.showAndWait();
     }
 
     @FXML
-    void Q3(ActionEvent event) {
-
+    void Q3(ActionEvent event) throws IOException {
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../vues/VueQ3.fxml"));
+    	Parent root1 = (Parent) fxmlLoader.load();
+    	Stage stage = new Stage();
+    	stage.setScene(new Scene(root1));
+    	stage.showAndWait();
     }
 
     @FXML
-    void Q4(ActionEvent event) {
-
+    void Q4(ActionEvent event) throws IOException {
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../vues/VueQ4.fxml"));
+    	Parent root1 = (Parent) fxmlLoader.load();
+    	Stage stage = new Stage();
+    	stage.setScene(new Scene(root1));
+    	stage.showAndWait();
     }
 
     @FXML
-    void Q5(ActionEvent event) {
-
+    void Q5(ActionEvent event) throws IOException {
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../vues/VueQ5.fxml"));
+    	Parent root1 = (Parent) fxmlLoader.load();
+    	Stage stage = new Stage();
+    	stage.setScene(new Scene(root1));
+    	stage.showAndWait();
     }
 
     @FXML
     void quitter(ActionEvent event) {
     	this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		this.stage.close();
+        this.stage.close();
     }
 }
